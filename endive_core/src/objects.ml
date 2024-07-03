0a96c1b4b8e9aea2a0d161606fd418d63f8dbdd3
sstern <solal.stern@ens-lyon.fr>
2024-07-03 12:21:52 +0900

    Add id-blind equality between objects

--- endive_core/src/objects.ml
+++ endive_core/src/objects.ml
@@ -10,3 +10,12 @@ let rec get_holes = function
   | T (_, _, children) -> List.concat (List.map get_holes children)
   | R (_, _, pattern, result) -> get_holes pattern @ get_holes result
   | C (_, obj, rule) -> get_holes obj @ get_holes rule
+
+let rec equals a b =
+  match (a, b) with
+  | H (_, name), H (_, name') -> name = name'
+  | T (_, f, children), T (_, f', children') ->
+      f = f' && List.for_all2 equals children children'
+  | R (_, v, l, r), R (_, v', l', r') -> v = v' && equals l l' && equals r r'
+  | C (_, l, r), C (_, l', r') -> equals l l' && equals r r'
+  | _ -> false
