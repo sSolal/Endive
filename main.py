5599f538e716ab3443fc371ba0c7728a09d1bb91
sstern <solal.stern@ens-lyon.fr>
2025-11-25 12:57:38 +0100

    Allow execution of a file by path

--- main.py
+++ main.py
@@ -8,10 +8,21 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", action="store_true")
     parser.add_argument("--test", action="store_true")
+    parser.add_argument("file", nargs="?", help="Path to .end file to execute")
 
     args = parser.parse_args()
     if args.test:
         integration.run(silent=not args.debug)
         exit(0)
+
     app = Cli(debug=args.debug)
-    app.run()
+
+    if args.file:
+        with open(args.file, 'r') as f:
+            for line in f:
+                if line.strip() == "":
+                    continue
+                print(line.strip())
+                app.process(line.strip())
+    else:
+        app.run()
